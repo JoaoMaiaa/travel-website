abea5ffbfbbad0f24469f16193ec1046172e5df7
João Maia <15joaomaia@gmail.com>
2022-04-14 18:06:28 -0300

    colocada as informaçoes na secao principal

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('.navbar-menu')
+let burger = document.querySelector('.navbar-burger')
+
+burger.addEventListener('click', ()=>{
+    menu.classList.toggle('is-active')
+    burger.classList.toggle('is-active')
+})
+
